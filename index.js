fe4df43160d665320c66c7e03194fb7dfa5e6d25
Petar-03 <petar_ganchev03@abv.bg>
2026-06-06 16:37:09 +0300

    displaying products on the page + star rating logic
    Displayed all products on the home page + implemented star rating logic per each product. Aslo made the design for the products in css

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const products = [
     title: "BMW Запалителни свещи",
     brand: "bmw",
     price: 19.99,
-    rating: 4,
+    rating: 4.5,
     image: "img/bmwParts/bmw-sweshti.jpg",
     description: "Комплект запалителни свещи, подходящ за BMW модели.",
   },
@@ -23,7 +23,7 @@ const products = [
     title: "BMW Спирачни дискове",
     brand: "bmw",
     price: 42.93,
-    rating: 4,
+    rating: 4.6,
     image: "img/bmwParts/bmw-diskowe.jpg",
     description: "Комплект спирачни дискове за BMW автомобили.",
   },
@@ -32,7 +32,7 @@ const products = [
     title: "BMW Амортисьори",
     brand: "bmw",
     price: 261.83,
-    rating: 5,
+    rating: 4.8,
     image: "img/bmwParts/bmw-amortisiori.jpg",
     description: "Комплект амортисьори за BMW автомобили.",
   },
@@ -43,7 +43,7 @@ const products = [
     title: "Mercedes Запалителни свещи",
     brand: "mercedes",
     price: 21,
-    rating: 5,
+    rating: 4.3,
     image: "img/mercedesParts/mercedes-sweshti.jpg",
     description: "Комплект запалителни свещи за стабилна работа на двигателя.",
   },
@@ -52,7 +52,7 @@ const products = [
     title: "Mercedes Спирачни накладки",
     brand: "mercedes",
     price: 94.99,
-    rating: 5,
+    rating: 4,
     image: "img/mercedesParts/mercedes-nakladki.jpg",
     description: "Комплект спирачни накладки за Mercedes автомобили.",
   },
@@ -61,7 +61,7 @@ const products = [
     title: "Mercedes Спирачни дискове",
     brand: "mercedes",
     price: 49.57,
-    rating: 3,
+    rating: 3.5,
     image: "img/mercedesParts/mercedes-diskowe.jpg",
     description: "Комплект спирачни дискове за Mercedes автомобили.",
   },
@@ -70,7 +70,7 @@ const products = [
     title: "Mercedes амортисьори ",
     brand: "mercedes",
     price: 19.99,
-    rating: 4,
+    rating: 4.6,
     image: "img/mercedesParts/mercedes-amortisiori.jpg",
     description:
       "Комплект амортисьори за Mercedes автомобили за добра стабилност на пътя.",
@@ -82,7 +82,7 @@ const products = [
     title: "Audi Запалителни свещи",
     brand: "audi",
     price: 30.99,
-    rating: 4,
+    rating: 4.7,
     image: "img/audiParts/audi-sweshti.jpg",
     description: "Комплект запалителни свещи за стабилна работа на двигателя.",
   },
@@ -91,7 +91,7 @@ const products = [
     title: "Audi Спирачни накладки",
     brand: "audi",
     price: 40.9,
-    rating: 4,
+    rating: 5,
     image: "img/audiParts/audi-nakladki.jpg",
     description: "Комплект спирачни накладки за Audi.",
   },
@@ -100,7 +100,7 @@ const products = [
     title: "Audi Спирачни дискове",
     brand: "audi",
     price: 52.7,
-    rating: 5,
+    rating: 4,
     image: "img/audiParts/audi-diskowe.jpg",
     description: "Комплект спирачни дискове за стабилно и сигурно спиране.",
   },
@@ -109,7 +109,7 @@ const products = [
     title: "Audi Амортисьори",
     brand: "audi",
     price: 328.8,
-    rating: 5,
+    rating: 4.3,
     image: "img/audiParts/audi-amortisiori.jpg",
     description: "Комплект спортни амортисьори за Audi автомобили",
   },
@@ -120,7 +120,7 @@ const products = [
     title: "Subaru Запалителни свещи",
     brand: "subaru",
     price: 23.99,
-    rating: 5,
+    rating: 4.8,
     image: "img/subaruParts/subaru-sweshti.jpg",
     description: "Комплект запалителни свещи, подходящи за Subaru модели.",
   },
@@ -129,7 +129,7 @@ const products = [
     title: "Subaru Спирачни накладки",
     brand: "subaru",
     price: 30.5,
-    rating: 4,
+    rating: 4.7,
     image: "img/subaruParts/subaru-nakladki.jpg",
     description: "Комплект спирачни накладки за Subaru.",
   },
@@ -138,7 +138,7 @@ const products = [
     title: "Subaru Спирачни дискове",
     brand: "subaru",
     price: 129.99,
-    rating: 3,
+    rating: 3.3,
     image: "img/subaruParts/subaru-diskowe.jpg",
     description:
       "Комплект спирачни дискове за надеждно спиране и контрол Subaru.",
@@ -148,7 +148,7 @@ const products = [
     title: "Subaru Амортисьори",
     brand: "subaru",
     price: 250,
-    rating: 4,
+    rating: 4.8,
     image: "img/subaruParts/subaru-amortisiori.jpg",
     description: "Комплект спортно окачване за Subaru.",
   },
@@ -159,7 +159,7 @@ const products = [
     title: "Toyota Запалителни свещи",
     brand: "toyota",
     price: 21.99,
-    rating: 4,
+    rating: 4.9,
     image: "img/toyotaParts/toyota-sweshti.jpg",
     description: "Комплект запалителни свещи за Toyota автомобили.",
   },
@@ -168,7 +168,7 @@ const products = [
     title: "Toyota Спирачни накладки",
     brand: "toyota",
     price: 32.7,
-    rating: 5,
+    rating: 4.5,
     image: "img/toyotaParts/toyota-nakladki.jpg",
     description: "Комплект Toyota накладки за моментално спиране.",
   },
@@ -177,7 +177,7 @@ const products = [
     title: "Toyota Спирачни дискове",
     brand: "toyota",
     price: 27.5,
-    rating: 5,
+    rating: 4.4,
     image: "img/toyotaParts/toyota-diskowe.jpg",
     description: "Комплект спирачни дискове за Toyota модели.",
   },
@@ -186,8 +186,129 @@ const products = [
     title: "Toyota Амортисьори",
     brand: "toyota",
     price: 200,
-    rating: 5,
+    rating: 4.6,
     image: "img/toyotaParts/toyota-amortisiori.jpg",
     description: "Комплект надеждени амортисьори за Toyota автомобили.",
   },
 ];
+
+//Display products on the page
+const productsGrid = document.querySelector(".products-grid");
+
+function renderProducts(productsToShow) {
+  productsGrid.innerHTML = "";
+
+  productsToShow.forEach(function (product) {
+    const productCard = document.createElement("article");
+    productCard.classList.add("product-card");
+    productsGrid.appendChild(productCard);
+
+    const imageBox = document.createElement("div");
+    imageBox.classList.add("product-image-box");
+    productCard.appendChild(imageBox);
+
+    const productImage = document.createElement("img");
+    productImage.src = product.image;
+    productImage.alt = product.title;
+    productImage.classList.add("product-img");
+
+    imageBox.appendChild(productImage);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+    productCard.appendChild(productInfo);
+
+    const productTitle = document.createElement("h3");
+    productTitle.classList.add("product-title");
+    productTitle.textContent = product.title;
+    productInfo.appendChild(productTitle);
+
+    const productPrice = document.createElement("p");
+    productPrice.classList.add("product-price");
+    productPrice.textContent = product.price + " EUR";
+    productInfo.appendChild(productPrice);
+
+    const productRating = document.createElement("div");
+    productRating.classList.add("product-rating");
+    productInfo.appendChild(productRating);
+
+    const stars = createRatingStars(product.rating);
+    const ratingNumber = document.createElement("span");
+    ratingNumber.classList.add("rating-number");
+    ratingNumber.textContent = `(${product.rating})`;
+    productRating.appendChild(stars);
+    productRating.appendChild(ratingNumber);
+
+    const productActions = document.createElement("div");
+    productActions.classList.add("product-actions");
+    productInfo.appendChild(productActions);
+
+    const detailsButton = document.createElement("button");
+    detailsButton.classList.add("details-btn");
+    detailsButton.setAttribute("product-id", product.id);
+    detailsButton.textContent = "Детайли";
+    productActions.appendChild(detailsButton);
+
+    const favoriteButton = document.createElement("button");
+    favoriteButton.classList.add("favorite-btn");
+    favoriteButton.setAttribute("product-id", product.id);
+    favoriteButton.setAttribute("aria-label", "Добави в любими");
+    productActions.appendChild(favoriteButton);
+
+    const favoriteIcon = document.createElement("i");
+    favoriteIcon.classList.add("bi");
+    favoriteIcon.classList.add("bi-heart");
+    favoriteButton.appendChild(favoriteIcon);
+  });
+}
+renderProducts(products);
+
+//Stars count on each product
+function createRatingStars(rating) {
+  const ratingStars = document.createElement("div");
+  ratingStars.classList.add("stars");
+
+  if (rating >= 5) {
+    ratingStars.innerHTML = `
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+    `;
+  } else if (rating > 4 && rating < 5) {
+    ratingStars.innerHTML = `
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-half"></i>
+    `;
+  } else if (rating === 4) {
+    ratingStars.innerHTML = `
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star"></i>
+    `;
+  } else if (rating > 3 && rating < 4) {
+    ratingStars.innerHTML = `
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-half"></i>
+      <i class="bi bi-star"></i>
+    `;
+  } else if (rating === 3) {
+    ratingStars.innerHTML = `
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star-fill"></i>
+      <i class="bi bi-star"></i>
+      <i class="bi bi-star"></i>
+    `;
+  }
+
+  return ratingStars;
+}
